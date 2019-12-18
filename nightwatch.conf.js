5a48c9c3d1b665a6c0fdfd40016ee435cd8b6456
manuel12 <manuelpinedacabeza@gmail.com>
2019-12-18 10:42:44 +0100

    Updated file in order to have chromedriver as a var and not need nightwatch.json

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,6 +1,38 @@
+require('dotenv').config();
 const chromedriver = require("chromedriver");
-module.exports = (function (settings) {
-  settings.test_workers = false;
-  settings.webdriver.server_path = chromedriver.path;
-  return settings;
-})(require("./nightwatch.json"));
+
+module.exports = {
+  "src_folders" :         ["tests"],
+  "page_objects_path" :   ["page_objects"],
+  "globals_path"  :       "globalsModule.js",
+
+  "webdriver" : {
+    "start_process": true,
+    "server_path": chromedriver.path,
+    "cli_args": [
+      "--log", "debug"
+    ],
+    "port": 9515
+  },
+
+  "test_settings" : {
+    "default" : {
+      "screenshots" : {
+        "enabled": true,
+        "on_failure": true,
+        "on_error": true,
+        "path": "tests_output/screenshots"
+        },
+      "desiredCapabilities": {
+        "browserName": "chrome"
+      }
+    },
+    "chrome": {
+        "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true,
+        "acceptSslCerts": true
+      }
+    }
+  }
+}
